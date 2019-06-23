8d97f35b032936b0001d1b491450e46404ab1b23
Grzegorz Kaźmierczak <g.p.kazmierczak@gmail.com>
2019-06-23 15:28:33 +0200

    New functionality: add comment

--- src/main/java/pl/sda/eventservice/controller/EventController.java
+++ src/main/java/pl/sda/eventservice/controller/EventController.java
@@ -2,16 +2,29 @@ package pl.sda.eventservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pl.sda.eventservice.controller.dto.CommentDto;
+import pl.sda.eventservice.controller.dto.UserDto;
 import pl.sda.eventservice.service.EventService;
+
+import javax.validation.Valid;
 
 @RestController
 public class EventController {
 
     EventService eventService;
 
+
     @Autowired
-    public EventController(EventService eventService){
+    public EventController(EventService eventService) {
         this.eventService = eventService;
+    }
+
+    @PostMapping("addComment")
+    public void addComment(@ModelAttribute("user") @Valid CommentDto commentDto){
+        eventService.addCommentToEvent(commentDto);
     }
 }
---
+++ src/main/java/pl/sda/eventservice/controller/dto/CommentDto.java
@@ -0,0 +1,11 @@
+package pl.sda.eventservice.controller.dto;
+
+import lombok.Data;
+
+@Data
+public class CommentDto {
+
+    private String content;
+    private String author;
+
+}
